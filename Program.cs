4d41ac5cc362466115d4260980e10ece0019631e
DESKTOP-JCH77E3\Ananasik <kravchenko.s1111@gmail.com>
2021-03-31 10:56:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module_2_Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = new Starter();
+            start.Run();
+        }
+    }
+}
